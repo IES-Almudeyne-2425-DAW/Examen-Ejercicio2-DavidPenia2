f295a33273baba8ed71c9dcfc634cfb28227b1a1
David <davidpefierro@gmail.com>
2024-11-06 08:36:17 +0100

    Modificación archivo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo{
-public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+public aa static void main(String[] args) {
+    System.out.println("Holaaa Mundo");
+    aaa
 }
 }
